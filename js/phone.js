006bb3ae519e9259368b230d8df63303420e2a31
Ananda Das <65402344+Ananda1405@users.noreply.github.com>
2023-08-27 22:04:57 +0600

    implement search functionality and display search result

--- js/phone.js
+++ js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () => {
-  const res = await fetch("https://openapi.programming-hero.com/api/phones?search=iphone");
+const loadPhone = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
   // console.log(phones);
@@ -10,11 +10,15 @@ const displayPhones = (phones) => {
   // console.log(phones);
 
   const phoneContainer = document.getElementById('phone-container');
+    //clar phone container cards before adding new cards
+    phoneContainer.textContent = '';
+
+
   phones.forEach((phone) => {
-    console.log(phone);
+    // console.log(phone);
     //2 create a div
     const phoneCard = document.createElement("div");
-    phoneCard.classList = "card w-96 bg-gray-100 shadow-xl";
+    phoneCard.classList = "card bg-gray-100 p-4 shadow-xl";
     //3 set inner html
     phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
@@ -31,4 +35,12 @@ const displayPhones = (phones) => {
   });
 };
 
-loadPhone();
+// handle Search button
+const handleSearch = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
+// loadPhone();
